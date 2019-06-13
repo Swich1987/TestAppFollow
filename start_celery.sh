1da18f9d560d42608aa8ef3bf8a9b23141251453
Swich1987 <swich1987@mail.ru>
2019-06-13 12:50:28 +0300

    Fix bug with django runned from celey, updated django to more secure version

--- start_celery.sh
+++ start_celery.sh
@@ -5,5 +5,6 @@ rm -f ./celerybeat.pid
 rm -f ./celerybeat-schedule
 rm -f ./celeryd.pid
 echo "Start periodic parsing..."
+cat ./hackertest/settings.ini | grep "UPDATE_PARSING_SEC"
 screen -dmS worker celery -A hackertest.celery_tasks.tasks worker --loglevel INFO
 celery -A hackertest.celery_tasks.tasks beat --loglevel INFO
