77bacfa5c384cd092c73ca4851df01a283f01fe2
Swich1987 <swich1987@mail.ru>
2019-06-14 14:15:21 +0300

    Update to better logging control on celery

--- hackertest/celery_tasks/settings.py
+++ hackertest/celery_tasks/settings.py
@@ -1,22 +1,21 @@
 """ Celery settings module.
 
-Most of settings are extracted from settings.ini
+All settings are extracted from settings.ini
 
 app - celery app, which can me imported to create celery's task
 
 UPDATE_PARSING_SEC - extracted from file. Time between parsing site.
 SOFT_TIME_LIMIT - celery setting for max limit per task.
+LOG_LEVEL - just log level for all loggers from logging module
 """
 import configparser
-import logging
 import os
 import sys
+import logging
 from logging.handlers import RotatingFileHandler
 
 from celery import Celery
 from celery.signals import after_setup_logger
-
-logger = logging.getLogger(__name__)
 
 _path_to_settings = os.path.join(sys.path[0], 'hackertest', 'settings.ini')
 
@@ -28,6 +27,7 @@ app.conf.timezone = _config['CELERY']['TIMEZONE']
 
 UPDATE_PARSING_SEC = _config.getfloat('CELERY', 'UPDATE_PARSING_SEC')
 SOFT_TIME_LIMIT = _config.getint('CELERY', 'SOFT_TIME_LIMIT')
+LOG_LEVEL = getattr(logging, _config['CELERY']['LOG_LEVEL'])
 
 
 @after_setup_logger.connect
@@ -36,10 +36,14 @@ def _setup_loggers(logger, *args, **kwargs):
     max_bytes = 1024 * _config.getint('CELERY', 'LOG_FILE_SIZE_KBYTES')
     backup_count = _config.getint('CELERY', 'LOG_FILE_BACKUP_COUNT')
 
-    fh = RotatingFileHandler(filename=_config['CELERY']['LOG_FILE_NAME'],
-                             maxBytes=max_bytes, backupCount=backup_count)
+    file_handler = RotatingFileHandler(filename=_config['CELERY']['LOG_FILE_NAME'],
+                                       maxBytes=max_bytes, backupCount=backup_count)
+    stream_handler = logger.handlers[0]
 
     format_str = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     formatter = logging.Formatter(format_str)
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
+    for handler in (file_handler, stream_handler):
+        handler.setFormatter(formatter)
+        handler.setLevel(LOG_LEVEL)
+
+    logger.addHandler(file_handler)
--- start_celery.sh
+++ start_celery.sh
@@ -4,7 +4,14 @@ echo "Clear celery files."
 rm -f ./celerybeat.pid
 rm -f ./celerybeat-schedule
 rm -f ./celeryd.pid
-echo "Start periodic parsing..."
+
+CELERY_LOG_LEVEL=`cat ./hackertest/settings.ini | grep "LOG_LEVEL = " |
+                 cut -d ' ' -f 3 | sed "s/\\r//g"`
+echo "CELERY LOG LEVEL = $CELERY_LOG_LEVEL"
 cat ./hackertest/settings.ini | grep "UPDATE_PARSING_SEC"
-screen -dmS worker celery -A hackertest.celery_tasks.tasks worker --loglevel INFO
-celery -A hackertest.celery_tasks.tasks beat --loglevel INFO
+echo "You can change settings in hackertest/settings.ini"
+
+echo "Start periodic parsing..."
+screen -dmS worker celery -A hackertest.celery_tasks.tasks worker \
+       --loglevel $CELERY_LOG_LEVEL
+celery -A hackertest.celery_tasks.tasks beat --loglevel $CELERY_LOG_LEVEL
